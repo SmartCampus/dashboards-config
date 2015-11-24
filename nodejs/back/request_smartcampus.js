5836ffaf1d867bb76864c55d1214476c685da8b2
Marc Karassev <marc.karassev@etu.unice.fr>
2015-11-24 08:57:57 +0100

    PD-26 comments again

--- nodejs/back/request_smartcampus.js
+++ nodejs/back/request_smartcampus.js
@@ -1,3 +1,9 @@
+/**
+ * @author Marc Karassev
+ *
+ * Module responsible for querying SmartCampus' data API.
+ */
+
 var http = require("http"),
 	SMARTCAMPUS_HOST = "http://smartcampus.unice.fr",
 	SENSORS_PATH = "/sensors";
@@ -6,6 +12,12 @@ function error(e, where) {
  	console.log("Got error in", where, ": " + e.message);
 }
 
+/**
+ * Retrieves all sensors from SmartCampus.
+ * 
+ * @param  {Function} callback function to call when job is finished with
+ *                             the body response from the API call
+ */
 function getAllSensors(callback) {
 	var url = SMARTCAMPUS_HOST + SENSORS_PATH;
 
@@ -17,6 +29,15 @@ function getAllSensors(callback) {
 	});
 }
 
+/**
+ * Retrieves the last data from a specific sensor.
+ * 
+ * @param  {[string]}	name 		the sensor's name
+ * @param  {[boolean]}  convert 	whether the timestamps should be converted
+ *                               	into "human readable" dates
+ * @param  {Function}	callback	function to call when job is finished with
+ *                             		the body response from the API call
+ */
 function getLastSensorData(name, convert, callback) {
 	var url = SMARTCAMPUS_HOST + SENSORS_PATH + "/" + name + "/data" + "/last" + "?convert=" + convert;
 
@@ -28,6 +49,18 @@ function getLastSensorData(name, convert, callback) {
 	});
 }
 
+/**
+ * Retrieves sensor data.
+ * 
+ * @param  {[string]}   name     	the sensor's name
+ * @param  {[string]}   date   		Date of the wished data or date range:
+ *                               	YYYY-MM-DD hh:mm:ss[/YYYY-MM-DD hh:mm:ss]
+ *                               	if no date is given, all sensor data will be returned
+ * @param  {[boolean]}   convert 	whether the timestamps should be converted
+ *                               	into "human readable" dates
+ * @param  {Function} callback 		function to call when job is finished with
+ *                             		the body response from the API call
+ */
 function getSensorData(name, date, convert, callback) {
 	var url = SMARTCAMPUS_HOST + SENSORS_PATH + "/" + name + "/data?convert=" + convert + (date? "&date=" + date : "");
 
