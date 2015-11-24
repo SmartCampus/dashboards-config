e92c522666982d8f83177cc6d4a38ef2c95fe768
Marc Karassev <marc.karassev@etu.unice.fr>
2015-11-24 07:34:48 +0100

    moving request_smartcampus tests into its file

--- nodejs/back/app.js
+++ nodejs/back/app.js
@@ -1,24 +1,3 @@
 var requestSmartcampus = require("./request_smartcampus");
 
-function logRes(req, res) {
-  res.on('data', function (chunk) {
-    console.log("********** received chunk from", req, "**********\n" + chunk);
-  });
-  res.on('end', function () {
-    console.log("********** end of", req, "**********");
-  });
-}
-
-requestSmartcampus.getAllSensors(function (res) {
-	logRes("getAllSensors", res);
-});
-requestSmartcampus.getLastSensorData("DOOR_443", false, function (res) {
-	logRes("getLastSensorData for DOOR_443", res);
-});
-requestSmartcampus.getSensorData("DOOR_443", "2015-10-14 18:00:11", false, function (res) {
-	logRes("getSensorData for DOOR_443 at 2015-10-14 18:00:11", res);
-});
-requestSmartcampus.getSensorData("DOOR_443", "2015-09-01 00:00:00/2015-10-01 00:00:00",
-	true, function (res) {
-		logRes("getSensorData for DOOR_443 between 2015-09-01 00:00:00 and 2015-10-01 00:00:00", res);
-});
+requestSmartcampus.test();
--- nodejs/back/request_smartcampus.js
+++ nodejs/back/request_smartcampus.js
@@ -39,8 +39,36 @@ function getSensorData(name, date, convert, callback) {
 	});
 }
 
+// Test
+
+function test() {
+	function logRes(req, res) {
+		res.on('data', function (chunk) {
+		console.log("********** received chunk from", req, "**********\n" + chunk);
+		});
+		res.on('end', function () {
+		console.log("********** end of", req, "**********");
+		});
+	}
+
+	getAllSensors(function (res) {
+		logRes("getAllSensors", res);
+	});
+	getLastSensorData("DOOR_443", false, function (res) {
+		logRes("getLastSensorData for DOOR_443", res);
+	});
+	getSensorData("DOOR_443", "2015-10-14 18:00:11", false, function (res) {
+		logRes("getSensorData for DOOR_443 at 2015-10-14 18:00:11", res);
+	});
+	getSensorData("DOOR_443", "2015-09-01 00:00:00/2015-10-01 00:00:00",
+		true, function (res) {
+			logRes("getSensorData for DOOR_443 between 2015-09-01 00:00:00 and 2015-10-01 00:00:00", res);
+	});
+}
+
 // Exports
 
 exports.getAllSensors = getAllSensors;
 exports.getLastSensorData = getLastSensorData;
 exports.getSensorData = getSensorData;
+exports.test = test;
