e4f49b54fbf0338bfda5615b22fc95f264b3897a
Quentin cornevin <qcornevin@gmail.com>
2015-11-25 14:26:57 +0100

    De la doc propre

--- nodejs/back/sensors_information.js
+++ nodejs/back/sensors_information.js
@@ -7,22 +7,24 @@
 var requestSmartcampus = require("./request_smartcampus");
 
 /**
- * This method will retrieve
+ * This method will retrieve all the data of the smart campus API for the given office and for the
+ * given interval of date. If there is no date, then it retrieves all the data.
  *
- * @param date
- * @param officeNumber
- * @param callback
+ * @param   {string}    date            Interval of date of the wished data
+ * @param   {integer}   officeNumber    The number of the office where we want the data
+ * @param   {response}  response        Response of the request exposed in the route
+ *
  */
-function getDeskTemperature(date, officeNumber ,callback) {
+function getDeskTemperature(date, officeNumber, response) {
         requestSmartcampus.getSensorData("TEMP_" + officeNumber + "V", date, false, function (res) {
         var stringData = ""
 
         res.on("data", function(chunck) {
            stringData += chunck;
-        })
+        });
+
         res.on("end" , function() {
             var tempPerTime = JSON.parse(stringData);
-
             var responseInGoodFormat = {"data": []};
 
             for(var i in tempPerTime.values) {
@@ -32,22 +34,28 @@ function getDeskTemperature(date, officeNumber ,callback) {
                 responseInGoodFormat.data.push(temperaturePerTime);
             }
 
-            callback.send(responseInGoodFormat);
+            response.send(responseInGoodFormat);
         })
     });
 }
 
-
-function getCampusTemperature(date, callback) {
+/**
+ * This method will retrieve the exterior temperature of the campus for the given interval of date.
+ *
+ * @param   {string}    date        Interval of date where we want the data
+ * @param   {response}  response    Response of the request exposed in the route
+ *
+ */
+function getCampusTemperature(date, response) {
     requestSmartcampus.getSensorData("TEMP_CAMPUS", date, false, function (res) {
-        var stringData = ""
+        var stringData = "";
 
         res.on("data", function(chunck) {
             stringData += chunck;
-        })
+        });
+
         res.on("end" , function() {
             var tempPerTime = JSON.parse(stringData);
-
             var responseInGoodFormat = {"data": []};
 
             for(var i in tempPerTime.values) {
@@ -57,12 +65,15 @@ function getCampusTemperature(date, callback) {
                 responseInGoodFormat.data.push(temperaturePerTime);
             }
 
-            callback.send(responseInGoodFormat);
+            response.send(responseInGoodFormat);
         })
     });
 }
 
-
+/**
+ * not used
+ * @param callback
+ */
 function getDoorsState(callback) {
     requestSmartcampus.getSensorData("DOOR443STATE", "", true, function (res) {
         var stringData = "";
@@ -80,46 +91,77 @@ function getDoorsState(callback) {
 
 
 /**
- * This method retieve all the data for the state of the window of the given office number
+ * This method retrieve all the data for the state of the window of the given office number
  *
- * @param {Function} callback     function to call when the job is finish with the body response
- *                                from the API call
- * @param officeNumber
+ * @param   {response}  response            Response of the request exposed in the route
+ * @param   {integer}   officeNumber        Office number where we want the data
+ *
  */
-function getWindowsState(callback, officeNumber) {
+function getWindowsState(response, officeNumber) {
     requestSmartcampus.getLastSensorData("WINDOW" + officeNumber + "STATE", false, function (res) {
         var stringData = "";
 
         res.on("data", function (chunck) {
             stringData += chunck;
         });
+
         res.on("end", function () {
             var json = JSON.parse(stringData);
             var windowState = {"state" : json.values[0].value};
-            callback.send(windowState);
+            response.send(windowState);
         });
     });
 }
 
-// TODO : si on veut normaliser faudrait que le capteur soit sur /AC443 et pas /AC_443 (meme synthase que la fenetre)
-function getAirConditionerState(callback, officeNumber) {
+/**
+ * This method retrieve the data with the last state of the air conditioner
+ *
+ * @param   {response}      response            Response of the request exposed in the route
+ * @param   {integer}       officeNumber        Number of the office where we want the data
+ *
+ *  TODO : si on veut normaliser faudrait que le capteur soit sur /AC443 et pas /AC_443 (meme synthase que la fenetre)
+ */
+function getAirConditionerState(response, officeNumber) {
     requestSmartcampus.getLastSensorData("AC_" + officeNumber + "STATE", false, function (res) {
         var stringData = "";
 
         res.on("data", function (chunck) {
             stringData += chunck;
         });
+
         res.on("end", function () {
             var json = JSON.parse(stringData);
             var windowState = {"state" : json.values[0].value};
-            callback.send(windowState);
+            response.send(windowState);
         });
     });
 }
 
 
-
+/**
+ * This method put the last state of air conditioner in the response in the route
+ *
+ * @type {getAirConditionerState}
+ */
 exports.getAirConditionerState = getAirConditionerState;
+
+/**
+ * This method put all the temperature for the interval of given date in the response in the route
+ *
+ * @type {getDeskTemperature}
+ */
 exports.getDeskTemperature = getDeskTemperature;
+
+/**
+ * This method put the last state of the window of the given office in the response in the route
+ *
+ * @type {getWindowsState}
+ */
 exports.getWindowsState = getWindowsState;
+
+/**
+ * This method put all the temperature for the interval of given date in the response in the route
+ *
+ * @type {getCampusTemperature}
+ */
 exports.getCampusTemperature = getCampusTemperature;
