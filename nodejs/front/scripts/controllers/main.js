a0b031657e1d4b3d70e6d4a11aa83b20492b7373
Garance <garance.vallat@gmail.com>
2015-11-25 15:37:27 +0100

    ready to receive API

--- nodejs/front/scripts/controllers/main.js
+++ nodejs/front/scripts/controllers/main.js
@@ -5,39 +5,17 @@
 /**
  * WINDOWS AT OFFICE 443
  **/
-var SM_windows = '{"values":[{"date":"1434630382","value":"2"},{"date":"1434630383","value":"0"},{"date":"1434630385","value":"5"},{"date":"1434630398","value":"1"},{"date":"1434630457","value":"6"},{"date":"1434630458","value":"0"},{"date":"1434630491","value":"2"},{"date":"1434630492","value":"6"},{"date":"1434630498","value":"3"},{"date":"1434630499","value":"0"},{"date":"1434630547","value":"3"},{"date":"1434630548","value":"0"},{"date":"1434630567","value":"1"},{"date":"1434630568","value":"3"},{"date":"1434630574","value":"4"},{"date":"1434630575","value":"0"},{"date":"1434630582","value":"2"},{"date":"1434630588","value":"6"},{"date":"1434630594","value":"8"},{"date":"1434630655","value":"3"},{"date":"1434630659","value":"3"},{"date":"1434630660","value":"1"},{"date":"1434630666","value":"3"},{"date":"1434698517","value":"1"},{"date":"1434718743","value":"2"},{"date":"1434718746","value":"0"},{"date":"1434720299","value":"2"},{"date":"1434720300","value":"0"},{"date":"1434720302","value":"1"},{"date":"1434970026","value":"2"},{"date":"1434970048","value":"0"},{"date":"1434970049","value":"3"}]}';
-var SM_AirConditionning = '{"values":[{"date":"1434630382","value":"100"},{"date":"1434630383","value":"10"},{"date":"1434630385","value":"100"},{"date":"1434630398","value":"25"},{"date":"1434630457","value":"45"},{"date":"1434630458","value":"56"},{"date":"1434630491","value":"34"},{"date":"1434630492","value":"65"},{"date":"1434630498","value":"35"},{"date":"1434630499","value":"45"},{"date":"1434630547","value":"20"},{"date":"1434630548","value":"20"},{"date":"1434630567","value":"10"},{"date":"1434630568","value":"0"},{"date":"1434630574","value":"0"},{"date":"1434630575","value":"20"},{"date":"1434630582","value":"25"},{"date":"1434630588","value":"30"},{"date":"1434630594","value":"0"},{"date":"1434630655","value":"5"},{"date":"1434630659","value":"12"},{"date":"1434630660","value":"15"},{"date":"1434630666","value":"18"},{"date":"1434698517","value":"31"},{"date":"1434718743","value":"32"},{"date":"1434718746","value":"45"},{"date":"1434720299","value":"100"},{"date":"1434720300","value":"100"},{"date":"1434720302","value":"100"},{"date":"1434970026","value":"90"},{"date":"1434970048","value":"90"},{"date":"1434970049","value":"93"}]}';
+var SM_windows = '[[100,2],[101,0],[102,1],[103,6],[104,0],[105,2],[106,6]]';
+var SM_AirConditioning = '[[100,100],[101,10],[102,100],[103,25],[104,45],[105,56],[106,34]]';
 
-
-var hourlyPeriod = [];
 var open_result = [];
 var ac_result = [];
 
 // Transform text to JSON
 hourlyWindow = JSON.parse(SM_windows);
-hourlyAC = JSON.parse(SM_AirConditionning);
-
-
-function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return time;
-}
-
-// Parse and add values of JSON in arrays
-for(var i = 0; i < hourlyWindow.values.length; i++)
-{
-    hourlyPeriod.push(timeConverter(hourlyWindow.values[i].date));
-    open_result.push(parseInt(hourlyWindow.values[i].value));
-    ac_result.push(parseInt(hourlyAC.values[i].value));
-}
+hourlyAC = JSON.parse(SM_AirConditioning);
+console.log(hourlyWindow);
+console.log(hourlyAC);
 
 //Success callback for retrieving the inside temperatures
 var firstSuccessInTemp = function(data) {
@@ -92,12 +70,10 @@ var firstSuccessInTemp = function(data) {
                 },
                 series: [{
                     name: 'office 443 temperature inside',
-                  //  data: [[1331028000456, 8], [1331031600900, 1], [1331035201230, 2]]
                     data: insideTemperature
                 },
                  {
                      name: 'office 443 temperature outside',
-                    // data: [[1331028000000, 5], [1331031600000, 6], [1331035200000, 4]]
                      data: outsideTemperature
                  }
                 ]
@@ -106,82 +82,94 @@ var firstSuccessInTemp = function(data) {
     };
 
     //We need to get the outside temperatures now, to build our whole graph.
-    retrieveData.askForTemp('campus/temperature', beginDate, endDate, secondSuccessInTemp);
+    retrieveData.askForSeries('campus/temperature', beginDate, endDate, secondSuccessInTemp);
 };
 var beginDate = '2015-10-14 8:00:11';
 var endDate = '2015-10-20 18:00:11';
+var place = 'office/443/';
 
 //First step of data retrieving : we get the inside temperatures
-retrieveData.askForTemp('office/443/temperature', beginDate, endDate, firstSuccessInTemp);
+retrieveData.askForSeries(place+'temperature', beginDate, endDate, firstSuccessInTemp);
 
 
 
-$(function () {
-    $('#c2').highcharts({
-        chart: {
-            type: 'column',
-            alignTicks: false
-        },
-        title: {
-            text: 'Window openings vs. A/C use'
-        },
-        subtitle: {
-            text: 'office 344'
-        },
-        xAxis: {
-            categories: hourlyPeriod,
-            crosshair: true
-        },
-        yAxis: [{ // Primary yAxis
-            labels: {
-                format: '{value}'
-            },
-            title: {
-                text: 'Openings'
-            },
-            allowDecimals: false,
-        }, { // Secondary yAxis
-            title: {
-                text: 'ACUse'
-            },
-            max : 100,
-            labels: {
-                format: '{value} %'
-            },
-            opposite: true
-        }],
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [{
-            name: 'Openings',
-            data: open_result,
-            yAxis: 0
-        }, {
-            name: 'ACUse',
-            data: ac_result,
-            yAxis: 1
-        }]
-    });
-});
+successForWindowCount = function(data) {
+    //Here, we must process if needed the data received, and then call the ac count
+
+console.log('in the window method');
+
+    //successForAcCount = function(data) {
+        $(function () {
+            $('#c2').highcharts({
+                chart: {
+                    type: 'column',
+                    alignTicks: false
+                },
+                title: {
+                    text: 'Window openings vs. A/C use'
+                },
+                subtitle: {
+                    text: 'office 344'
+                },
+                xAxis: {
+                    //  tickInterval: 24 * 3600 * 1000,
+                    type: 'category'
+                },
+                yAxis: [{ // Primary yAxis
+                    title: {
+                        text: 'ACUse'
+                    },
+                    max: 100,
+                    labels: {
+                        format: '{value} %'
+                    }
+                }, { // Secondary yAxis
+                    title: {
+                        text: 'Openings'
+                    },
+                    labels: {
+                        format: '{value}'
+                    },
+                    allowDecimals: false,
+                    opposite: true
+                }],
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'ACUse',
+                    data: hourlyAC,
+                    yAxis: 0
+                }, {
+                    name: 'WindowOpenings',
+                    data: hourlyWindow,
+                    yAxis: 1
+                }]
+            });
+        });
+    //};
+    //retrieveData.askForSeries(place+'ac_percentage', beginDate, beginDate, successForAcCount);
+
+};
+successForWindowCount();
+//retrieveData.askForSeries(place+'window_opening', beginDate, endDate, successForWindowCount);
 
 /**
  The boolean values for A/C and window :
  */
 
 var successForWindow = function(data) {
-    console.log('in success result');
     if (data.state == 'CLOSED') {
         document.getElementById('windowState').setAttribute("class", "label label-danger");
     }
@@ -196,8 +184,8 @@ var successForAC = function(data) {
         document.getElementById('climState').setAttribute("class", "label label-success");
 };
 
-retrieveData.askForStateNow('office/443/window_status', successForWindow);
-retrieveData.askForStateNow('office/443/ac_status', successForAC);
+retrieveData.askForStateNow(place+'window_status', successForWindow);
+retrieveData.askForStateNow(place+'ac_status', successForAC);
 
 /**
  * Datepikckers
@@ -216,7 +204,7 @@ $('#datetimepicker2').datetimepicker({
 $( "#refresh" ).click(function() {
     var from = $('#datetimepicker1').data('date');
     var to = $('#datetimepicker2').data('date');
-    retrieveData.askForTemp('office/443/temperature', from, to, firstSuccessInTemp);
+    retrieveData.askForTemp(place+'temperature', from, to, firstSuccessInTemp);
 
 });
 
--- nodejs/front/scripts/model/createDashboard.js
+++ nodejs/front/scripts/model/createDashboard.js
@@ -3,46 +3,46 @@
  */
 var retrieveData = (function () {
     return { //exposed to public
-        askForTemp: function (route, beginDate, endDate, successCB) {
-            console.log('before the get temperature for a timespan');
-            console.log('For route '+ route + ' : \nbegin : ' + beginDate + '\nend : '+endDate);
+        askForSeries: function (route, beginDate, endDate, successCB) {
+//            console.log('before the get temperature for a timespan');
+//            console.log('For route '+ route + ' : \nbegin : ' + beginDate + '\nend : '+endDate);
             $.get(serverURL + route, {date: beginDate+"/"+endDate})
                 .done(function (data) {
-                    console.log('got temps for a timespan');
+//                    console.log('got temps for a timespan');
                   //  alert("Data Loaded: " + data);
                     successCB(data);
                 })
                 .fail(function (data) {
-                    console.log(data);
-                    console.log('error in get temp for timespan');
+ //                   console.log(data);
+ //                   console.log('error in get temp for timespan');
                     //alert("error");
                 })
                 .always(function (data) {
-                    console.log('processed');
+   //                 console.log('processed');
                     //alert("finished");
                 });
-            console.log('after the get');
+     //       console.log('after the get');
         },
         askForStateNow: function (route, successStateCB) {
-            console.log('before the get window state');
+//            console.log('before the get window state');
 
             $.get(serverURL + route)
                 .done(function (data) {
-                    console.log('got window state');
-                    console.log(data);
+  //                  console.log('got window state');
+    //                console.log(data);
                //       alert("Data Loaded: " + data);
                     successStateCB(data);
                 })
                 .fail(function (data) {
-                    console.log(data);
-                    console.log('error in window state');
+      //              console.log(data);
+        //            console.log('error in window state');
                  //   alert("error");
                 })
                 .always(function (data) {
-                    console.log('processed');
+          //          console.log('processed');
                    // alert("finished");
                 });
-            console.log('after the get');
+//            console.log('after the get');
         }
     }
 }());
