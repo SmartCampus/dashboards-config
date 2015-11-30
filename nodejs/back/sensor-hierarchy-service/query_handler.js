3fa927960d5ed34f655c3675b15b2d6f7fc2e61d
Quentin cornevin <qcornevin@gmail.com>
2015-11-30 21:08:54 +0100

    PD-64 : Refractor finis

--- nodejs/back/sensor-hierarchy-service/query_handler.js
+++ nodejs/back/sensor-hierarchy-service/query_handler.js
@@ -7,46 +7,26 @@ function handleQuery(queries, res) {
     var sensorsArray = [];
 
     for(var query in queries) {
+        var queryResult = [];
         console.log(sensors.getSmartCampusSensors().length);
         var smartCampusSensors = sensors.getSmartCampusSensors();
         for(var iterator in smartCampusSensors) {
             var name = smartCampusSensors[iterator].getName();
             if(queries[query] === name.replace(/\s+/g, '')) {
                 for(var sensor in smartCampusSensors[iterator].getSensors()) {
-                    sensorsArray.push(sensors.getSmartCampusSensors()[iterator].getSensors()[sensor]);
+                    queryResult.push(sensors.getSmartCampusSensors()[iterator].getSensors()[sensor]);
                 }
             }
         }
+        sensorsArray.push(queryResult);
     }
-/*
+
     var result = sensorsArray.shift().filter(function(v) {
         return sensorsArray.every(function(a) {
             return a.indexOf(v) !== -1;
         });
-    }); */
-
-
- /*   var queryInArray = [];
-    for(var i in queries) {
-        queryInArray.push(queries[i]);
-    }
-
-    var arrays = [queryInArray, Sensors.getSmartCampusSensors()];
-    console.log(Sensors.getSmartCampusSensors());
-    console.log(queryInArray);
-    var result = arrays.shift().filter(function(v) {
-        return arrays.every(function(a) {
-            return a.indexOf(v) !== -1;
-        });
     });
-
-    for(var iter in Sensors.getSmartCampusSensors()) {
-        if(Sensors.getSmartCampusSensors()[iter] == result[0]) {
-
-        }
-    } */
-
-    res.send(sensorsArray);
+    res.send(result);
 }
 
 exports.handleQuery = handleQuery;
