a2b2f3880c1c9db7bd6c226adcc3dcb85bba8712
Marc Karassev <marc.karassev@etu.unice.fr>
2015-12-01 12:15:26 +0100

    màj scripts

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+
+cd "$parent_path"
 cd back
 npm install
 cd ../front
 npm install
+bower install --allow-root
 npm install -g forever
--- nodejs/start.sh
+++ nodejs/start.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+
+cd "$parent_path"
 forever start back/app.js
 forever start front/app.js
