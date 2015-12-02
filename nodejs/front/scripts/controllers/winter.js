4b5c280f1f6e4a48f45f4882f2f9b34ae7bdf2e5
Salah Bennour hamza <bennour@polytech.unice.fr>
2015-12-02 15:25:16 +0100

    PD-108 Création du graphe des températures. Pb : impossible d'allier un histogramme avec les 2 graphes. A résoudre.

--- nodejs/front/scripts/controllers/winter.js
+++ nodejs/front/scripts/controllers/winter.js
@@ -15,64 +15,121 @@ if (typeof beginDate == 'undefined' || typeof endDate == 'undefined') {
 var temperaturesArray = [];
 
 var firstSuccessInTemp = function (data, callback) {
-    temperaturesArray[0] = {"name": "inside temparature", "data": data.data};
+    temperaturesArray[0] = {"type": 'line', "name": "inside temparature", "data": data.data,  "yAxis": 1};
     callback();
 };
 
 var secondSuccessInTemp = function (data, callback) {
-    temperaturesArray[1] = {"name": "outside temparature", "data": data.data};
+    temperaturesArray[1] = {"type": 'line',"name": "outside temparature", "data": data.data,  "yAxis": 1};
     callback();
 };
 
-var toUpdate = false;
+/**
+var thirdSuccessInTemp = function (data, callback) {
+    temperaturesArray[2] = {"type": 'column',"name": "heater state" , "data": data.data,  "yAxis": 0};
+    callback();
+};**/
 
-function update(){
-    if(!toUpdate){
-        toUpdate = true;
+var toUpdate = 0;
+
+function updateCallback(){
+    if( toUpdate < 1){
+        toUpdate++;
     }else{
         drawLineChart();
+        toUpdate = 0;
     }
 }
 
 retrieveData.askForSeries('TEMP_443V/data', beginDate, endDate,
     function(data){
-        firstSuccessInTemp(data, update)
+        firstSuccessInTemp(data, updateCallback)
     }
 );
 
 retrieveData.askForSeries('TEMP_CAMPUS/data', beginDate, endDate,
     function(data) {
-        secondSuccessInTemp(data, update);
+        secondSuccessInTemp(data, updateCallback);
     }
 );
 
-
-
+/**
+retrieveData.askForSeriesWithParam('DOOR443STATE/data/splitlist', 'true', beginDate, endDate,
+    function (data) {
+        thirdSuccessInTemp(data, updateCallback);
+    }
+);**/
 
 var drawLineChart = function() {
 
     $('#c1').highcharts('StockChart', {
 
-        yAxis: {
+        yAxis:
+        [
+            { // Primary yAxis
+                min: 0,
 
-            title: {
-                text: 'Temperature (°C)'
+                title: {
+                    text: 'heater state',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+
+                labels: {
+                    format: '{value}',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+
+                style: {
+                    color: Highcharts.getOptions().colors[1]
+                },
+
+                opposite: false
             },
+            {
+                title: {
+                    text: 'Temperature (°C)',
+                    style: {
+                        color: Highcharts.getOptions().colors[3]
+                    }
+                },
 
-            minTickInterval: 1,
+                minTickInterval: 1,
 
-            plotLines: [{
-                value: 0,
-                color: 'red',
-                dashStyle: 'shortdash', //pointillé
-                width: 2,
-                label: { text: '0°C'}
-            }]
-        },
+                plotLines: [{
+                    value: 0,
+                    color: 'red',
+                    dashStyle: 'shortdash', //pointillé
+                    width: 2,
+                    label: { text: '0°C'}
+                }],
+
+                labels: {
+                    format: '{value} °C',
+                    style: {
+                        color: Highcharts.getOptions().colors[3]
+                    }
+                },
+
+                style: {
+                    color: Highcharts.getOptions().colors[3]
+                },
+
+                allowDecimals: false,
+                opposite: true
+            }
+        ],
 
         tooltip: {
-            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
-            valueDecimals: 2
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat:    '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
         },
 
         series: temperaturesArray
