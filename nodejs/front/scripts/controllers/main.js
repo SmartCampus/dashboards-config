5032671b5c03e7e4c50d1efad344aa11240482fd
Salah Bennour hamza <bennour@polytech.unice.fr>
2015-12-03 12:30:17 +0100

    PD-111 Interface de visualisation des capteurs hierarchisés et dynamiques

--- nodejs/front/scripts/controllers/main.js
+++ nodejs/front/scripts/controllers/main.js
@@ -1,77 +1,129 @@
-var besoins = [{key:1, value:"comparison"}, {key:2, value:"proportion"}, {key:3, value: "hierarchy"}, {key:4, value: "distribution"}];
-var capteurs = [
-    {
-        value: "Building Templiers",
-        bla: [
-            {
-                value: "Office 443",
-                nodes: [
-                    {
-                        value: "Temperature"
-                    },
-                    {
-                        value: "Noise"
-                    },
-                    {
-                        value: "Door"
-                    },
-                    {
-                        value: "Window"
-                    }
-                ]
-            },
-            {
-                value: "Office 444",
-                nodes: [
-                    {
-                        value: "Temperature"
-                    },
-                    {
-                        value: "Noise"
-                    },
-                    {
-                        value: "Door"
-                    },
-                    {
-                        value: "Window"
-                    }
-                ]
-            },
-            {
-                text: "Office 445",
-                nodes: [
-                    {
-                        value: "Temperature"
-                    },
-                    {
-                        value: "Noise"
-                    },
-                    {
-                        value: "Door"
-                    },
-                    {
-                        value: "Window"
-                    }
-                ]
-            }
-        ]
-    }];
+/**
+ * Algo
+ *
+ * recuperer le premier étage
+ *      si il a des directSensor
+ *          afficher
+ *      si il a des childContainer
+ *          mettre le name en lien
+ */
 
+var capteurs =
+    [
+        {
+            "name":"Templiers Ouest",
+            "sensors":[],
+            "childContainer":
+                [
+                    {
+                        "name":"4th floor",
+                        "sensors":[],
+                        "childContainer":
+                            [
+                                {
+                                    "name":"Coffee corner",
+                                    "sensors":[],
+                                    "childContainer":[],
+                                    "filters":["COFFEE","CAFE"],
+                                    "directSensor":["LIGHT_CAFE","Coffee_energy","Coffee_power","Window_Coffee","Coffee_machine_power","Coffee_machine_energy"]
+                                },
+                                {
+                                    "name":"Sous repartiteur",
+                                    "sensors":[],
+                                    "childContainer":[],
+                                    "filters":["MW"],
+                                    "directSensor":["MW_energy"]
+                                },
+                                {
+                                    "name":"Modalis corridor",
+                                    "sensors":[],
+                                    "childContainer":
+                                        [
+                                            {
+                                                "name":"Office 445",
+                                                "sensors":[],
+                                                "childContainer":[],
+                                                "filters":["445"],
+                                                "directSensor":[]
+                                            },
+                                            {
+                                                "name":"Office 444",
+                                                "sensors":[],
+                                                "childContainer":[],
+                                                "filters":["444"],
+                                                "directSensor":["LIGHT_444","TEMP_444"]
+                                            },
+                                            {
+                                                "name":"Office 443",
+                                                "sensors":[],
+                                                "childContainer":[],
+                                                "filters":["443"],
+                                                "directSensor":["HEATING_443","TEMP_443","AC_443","PRESENCE_443","DOOR_443","DOOR443STATE","TEMP_443V","WINDOW_443","WINDOW443STATE","AC_443STATE"]
+                                            }
+                                        ],
+                                    "filters":["Modalis","CORRIDOR"],
+                                    "directSensor":["NOISE_SPARKS_CORRIDOR","Window_Modalis"]
+                                }
+                            ],
+                        "filters":["SPARKS"],
+                        "directSensor":["DOOR_SPARKS"]
+                    }
+                ],
+            "filters":[],
+            "directSensor":[]
+        }
+    ];
+
+
+
+var current = capteurs;
+var previous = null;
+
+init();
 function init() {
+    explore();
+}
 
-    // update needed
-    for(var i = 0; i < besoins.length; i++){
-        var s = "<tr><td><div class=\"redips-drag square\">"+besoins[i].value+"</div></td></tr>";
-        $("#besoins").append(s);
+function explore() {
+    clean();
+
+    for(var i = 0; i < current.length; i++){
+        var s = "<div class=\"row\"> <a class=\"node\" id=\""+i+"\">"+current[i].name+"</a></div>"
+        $("#add-it").append(s);
     }
 
-    // update building captors
-    for(var i = 0; i < capteurs.length; i++){
-        if(capteurs[i].nodes != null){
-            var s = "<tr><td><div>"+capteurs[i].text+"</div></td></tr>";
-        }else{
-            var s = "<tr><td><div class=\"redips-drag square\">"+capteurs[i].value+"</div></td></tr>";
+    if (previous != null) {
+        for(var i = 0; i < previous.directSensor.length; i++){
+            var a = "<div class=\"row\"> <button class=\"drag\">"+previous.directSensor[i]+"</button></div>"
+            $("#add-it").append(a);
         }
-        $("#capteurs").append(s);
     }
 }
+
+function clean() {
+    $( "#add-it" ).empty();
+}
+
+
+/** click on node **/
+
+
+$(document).on('click', '.node', function(el) {
+    if(current == capteurs){
+        previous = null;
+    }else{
+        previous = current[parseInt(el.target.id)];
+    }
+    current = current[parseInt(el.target.id)].childContainer;
+    explore();
+});
+
+
+/** click on draggable element **/
+$( ".drag" ).click(function() {
+    alert("drag");
+});
+
+/** click on back **/
+$( "#goback" ).click(function() {
+});
