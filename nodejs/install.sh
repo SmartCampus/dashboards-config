a4e2c8e83955b5ae176f83d5dac3e23f32eb36c7
Marc Karassev <marc.karassev@etu.unice.fr>
2015-12-10 08:06:55 +0100

    màj scripts

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+echo "this script should be executed in root mode"
+echo "you should have npm ans bower installed"
+
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
 cd "$parent_path"
 cd back/format-data-service
 npm install
 cd ../sensor-containers-API
+npm install
+cd ../dashboard-generator
 npm install
 cd ../../front
 npm install
