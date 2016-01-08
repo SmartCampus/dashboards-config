60450ccf34d68b73d8b12edca2bf202371601cc0
Marc Karassev <marc.karassev@yahoo.fr>
2016-01-08 15:59:14 +0100

    PD-153 lien avec le front pour la génération du layout mais ça derpe

--- nodejs/front/scripts/model/generateLayout.js
+++ nodejs/front/scripts/model/generateLayout.js
@@ -1,12 +1,12 @@
 /**
  * Created by Garance on 05/01/2016.
  */
-    var halfnhalfLayout = '<div class="row">    <div id="left1" class="col-md-6 text-center"></div>    <div id="right1" class="col-md-6 text-center"></div>    </div>    <div class="row">    <div class="col-md-6" id="left2" style="height: 400px; min-width: 310px"></div>    <div class="col-md-6" id="right2" style="height: 400px; min-width: 310px"></div>   </div> ';
+//var halfnhalfLayout = '<div class="row">    <div id="left1" class="col-md-6 text-center"></div>    <div id="right1" class="col-md-6 text-center"></div>    </div>    <div class="row">    <div class="col-md-6" id="left2" style="height: 400px; min-width: 310px"></div>    <div class="col-md-6" id="right2" style="height: 400px; min-width: 310px"></div>   </div> ';
 
 var layouts = (function () {
     return { //exposed to public
         newLayout: function(layoutName, successCB, errorCB) {
-            /*$.post(genServer+layoutGen,
+            $.post(genServer+widgetGen,
                 {
                     job : "generateLayout",
                     config :
@@ -22,8 +22,8 @@ var layouts = (function () {
                 .fail(function (data) {
                     console.log('error in post gen layout');
                     errorCB();
-                }); */
-            successCB(halfnhalfLayout);
+                });
+            //successCB(halfnhalfLayout);
         }
 
     }
