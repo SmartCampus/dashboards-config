f8b3afedd0eccb51445542d9dff1a6f2227ae974
Garance <garance.vallat@gmail.com>
2016-01-27 17:06:21 +0100

    started layouts PD-178

--- nodejs/front/scripts/controllers/layoutChoice.js
+++ nodejs/front/scripts/controllers/layoutChoice.js
@@ -16,7 +16,7 @@ $("button").click(function(){
     if(id == 0){
         divideInTwoEgal();
     }else if(id == 1){
-        divideInTwoNotEgal();
+        divideInTwoThirds();
     }else if(id == 2){
         divideInThreeEgal();
     }else{
@@ -27,16 +27,16 @@ $("button").click(function(){
 
 function divideInTwoEgal() {
     var content = "<div class=\"row\">"+
-        "<div class=\"col-md-6\" style=\"min-height: 500px; background-color: #cf867e;\">.col-md-6</div>"+
-        "<div class=\"col-md-6\" style=\"min-height: 500px; background-color: #66afe9;\">.col-md-6</div>"+
+        "<div class=\"col-md-6\" style=\"min-height: 500px; background-color: rgba(207, 134, 126, 0.55);\">One half</div>"+
+        "<div class=\"col-md-6\" style=\"min-height: 500px; background-color: rgba(102, 175, 233, 0.55);\">One half</div>"+
         "</div>";
     $("#add-content").append(content);
 }
 
-function divideInTwoNotEgal() {
+function divideInTwoThirds() {
     var content = "<div class=\"row\">"+
-        "<div class=\"col-md-8\" style=\"min-height: 500px; background-color: #cf867e;\">.col-md-8</div>"+
-        "<div class=\"col-md-4\" style=\"min-height: 500px; background-color: #66afe9;\">.col-md-4</div>"+
+        "<div class=\"col-md-4\" style=\"min-height: 500px; background-color: rgba(207, 134, 126, 0.55);\">One third</div>"+
+        "<div class=\"col-md-8\" style=\"min-height: 500px; background-color: rgba(102, 175, 233, 0.55);\">Two thirds</div>"+
         "</div>";
     $("#add-content").append(content);
 }
@@ -44,9 +44,9 @@ function divideInTwoNotEgal() {
 function divideInThreeEgal() {
 
     var content = "<div class=\"row\">"+
-        "<div class=\"col-md-4\" style=\"min-height: 500px; background-color: #cf867e;\">.col-md-4</div>"+
-        "<div class=\"col-md-4\" style=\"min-height: 500px; background-color: #66afe9;\">.col-md-4</div>"+
-        "<div class=\"col-md-4\" style=\"min-height: 500px; background-color: #000000;\">.col-md-4</div>"+
+        "<div class=\"col-md-4\" style=\"min-height: 500px; background-color: rgba(207, 134, 126, 0.55);\">One third</div>"+
+        "<div class=\"col-md-4\" style=\"min-height: 500px; background-color: rgba(102, 175, 233, 0.55);\">One third</div>"+
+        "<div class=\"col-md-4\" style=\"min-height: 500px; background-color: rgba(112, 215, 0, 0.56);\">One third</div>"+
         "</div>";
     $("#add-content").append(content);
 }
@@ -54,9 +54,9 @@ function divideInThreeEgal() {
 function divideInThreeNotEgal() {
 
     var content = "<div class=\"row\">"+
-        "<div class=\"col-md-3\" style=\"min-height: 500px; background-color: #cf867e;\">.col-md-3</div>"+
-        "<div class=\"col-md-6\" style=\"min-height: 500px; background-color: #66afe9;\">.col-md-6</div>"+
-        "<div class=\"col-md-3\" style=\"min-height: 500px; background-color: #000000;\">.col-md-3</div>"+
+        "<div class=\"col-md-3\" style=\"min-height: 500px; background-color: rgba(207, 134, 126, 0.55) ;\">One third</div>"+
+        "<div class=\"col-md-6\" style=\"min-height: 500px; background-color: rgba(102, 175, 233, 0.55);\">Two thirds</div>"+
+        "<div class=\"col-md-3\" style=\"min-height: 500px; background-color: rgba(112, 215, 0, 0.56);\">One third</div>"+
         "</div>";
     $("#add-content").append(content);
 }
