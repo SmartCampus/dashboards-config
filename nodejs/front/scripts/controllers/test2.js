20a0360be6739118b4dfb50c3e18929d7b2d8f67
Salah Bennour hamza <bennour@polytech.unice.fr>
2016-02-23 10:47:20 +0100

    PD-236 Widgets drag and drop dans les layouts générés

--- nodejs/front/scripts/controllers/test2.js
+++ nodejs/front/scripts/controllers/test2.js
@@ -1,45 +1,167 @@
-/**
- * Created by salahbennour on 06/01/2016.
- */
-
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
-
-var gridster;
-
-function getRandomInt (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 $(function () {
 
-    gridster = $(".gridster ul").gridster({
-        widget_base_dimensions: [100, 100],
-        widget_margins: [5, 5],
-        helper: 'clone',
-        resize: {
-            enabled: true
-        }
-    }).data('gridster');
 
-    $('.js-resize-random').on('click', function () {
-        gridster.resize_widget(gridster.$widgets.eq(getRandomInt(0, 9)),
-            getRandomInt(1, 4), getRandomInt(1, 4))
+    var seriesOptions = [],
+        seriesCounter = 0,
+        names = ['MSFT', 'AAPL', 'GOOG'];
+
+    /**
+     * Create the chart when all data is loaded
+     * @returns {undefined}
+     */
+    function createChart() {
+
+        $('#c2').highcharts('StockChart', {
+
+            rangeSelector: {
+                selected: 4
+            },
+
+            yAxis: {
+                labels: {
+                    formatter: function () {
+                        return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                    }
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 2,
+                    color: 'silver'
+                }]
+            },
+
+            plotOptions: {
+                series: {
+                    compare: 'percent'
+                }
+            },
+
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                valueDecimals: 2
+            },
+
+            series: seriesOptions
+        });
+    }
+
+    $.each(names, function (i, name) {
+
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+
+            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+            // we keep a counter and create the chart when all the data is loaded.
+            seriesCounter += 1;
+
+            if (seriesCounter === names.length) {
+                createChart();
+            }
+        });
     });
 
-    $( ".draggable" ).draggable();
-    $( ".gridster " ).droppable({
-        drop: function( event, ui ) {
-            $( ".draggable" ).removeAttr("style");
-            $( ".draggable" ).removeAttr("class");
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=large-dataset.json&callback=?', function (data) {
 
-            console.log(ui.draggable);
+        // Create a timer
+        var start = +new Date();
 
-            gridster.add_widget('<li class="new">'+ui.context+'</li>', 1, 1);
+        // Create the chart
+        $('#c1').highcharts('StockChart', {
+            chart: {
+                events: {
+                    load: function () {
+                        if (!window.isComparing) {
+                            this.setTitle(null, {
+                                text: 'Built chart in ' + (new Date() - start) + 'ms'
+                            });
+                        }
+                    }
+                },
+                zoomType: 'x'
+            },
 
-        }
+            rangeSelector: {
+
+                buttons: [{
+                    type: 'day',
+                    count: 3,
+                    text: '3d'
+                }, {
+                    type: 'week',
+                    count: 1,
+                    text: '1w'
+                }, {
+                    type: 'month',
+                    count: 1,
+                    text: '1m'
+                }, {
+                    type: 'month',
+                    count: 6,
+                    text: '6m'
+                }, {
+                    type: 'year',
+                    count: 1,
+                    text: '1y'
+                }, {
+                    type: 'all',
+                    text: 'All'
+                }],
+                selected: 3
+            },
+
+            yAxis: {
+                title: {
+                    text: 'Temperature (°C)'
+                }
+            },
+
+            title: {
+                text: 'Hourly temperatures in Vik i Sogn, Norway, 2009-2015'
+            },
+
+            subtitle: {
+                text: 'Built chart in ...' // dummy text to reserve space for dynamic subtitle
+            },
+
+            series: [{
+                name: 'Temperature',
+                data: data.data,
+                pointStart: data.pointStart,
+                pointInterval: data.pointInterval,
+                tooltip: {
+                    valueDecimals: 1,
+                    valueSuffix: '°C'
+                }
+            }]
+
+        });
     });
-
 });
+
+$(function() {
+    $( "#sortable1, #sortable2" ).sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+});
+
+function dragStart(ev) {
+    ev.dataTransfer
+        .setData("ID",ev.target.getAttribute('id'));
+    ev.target.classList.add('dragging');
+}
+
+function dragOver(ev) {
+    ev.target.classList.remove('dragging');
+    return false;
+}
+
+
+function dragDrop(ev) {
+    var idelt = ev.dataTransfer.getData("ID");
+    ev.target
+        .appendChild(document.getElementById(idelt));
+}
