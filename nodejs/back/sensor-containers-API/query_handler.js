c4ad7c66dc1ab0877f41d7b57887171e74dca3c9
Quentin cornevin <qcornevin@gmail.com>
2016-02-24 10:47:55 +0100

    Conteur de capteur recursif

--- nodejs/back/sensor-containers-API/query_handler.js
+++ nodejs/back/sensor-containers-API/query_handler.js
@@ -114,7 +114,7 @@ function getContainerChild(name, callback) {
     var containerList = sensors.getContainers();
     for(var i in containerList) {
         if(containerList[i].getName().replace(/\s+/g, '') == name) {
-            var jsonResponse = {"name" : "Buildings",  childContainer : containerList[i].getChild()};
+            var jsonResponse = {"name" : "Buildings",  childContainer : containerList[i].toJSON()};
             callback(jsonResponse);
         }
     }
