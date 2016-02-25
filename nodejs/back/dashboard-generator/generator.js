7c202edb7174c0a25c12b1baa4d9941611bd4f9b
Marc Karassev <marc.karassev@yahoo.fr>
2016-02-25 10:02:56 +0100

    PD-256

--- nodejs/back/dashboard-generator/generator.js
+++ nodejs/back/dashboard-generator/generator.js
@@ -196,6 +196,13 @@ function readTemplateFile(templateFile, callback) {
     });
 }
 
+function generateMap(config, callback) {
+    console.log(config);
+    readTemplateFile("map.mustache", function (template) {
+        callback(null, Mustache.render(template, config));
+    });
+}
+
 // Exports
 
 exports.generateBoolean = generateBoolean;
@@ -205,3 +212,5 @@ exports.generateGraph = generateGraph;
 exports.generatePie = generatePie;
 
 exports.generateLayout = generateLayout;
+
+exports.generateMap = generateMap;
