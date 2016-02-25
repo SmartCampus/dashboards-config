65ac46fdcd26b2c36bf66935d0b8328fb51ab9e6
Salah Bennour hamza <bennour@polytech.unice.fr>
2016-02-25 14:54:21 +0100

    PD-254 Drag and drop des widgets : OK. Avec affichage des ombres.

--- nodejs/front/scripts/controllers/test2.js
+++ nodejs/front/scripts/controllers/test2.js
@@ -45,6 +45,99 @@ $(function () {
         });
     }
 
+    /**
+     * Load new data depending on the selected min and max
+     */
+    function afterSetExtremes(e) {
+
+        var chart = $('#container').highcharts();
+
+        chart.showLoading('Loading data from server...');
+        $.getJSON('https://www.highcharts.com/samples/data/from-sql.php?start=' + Math.round(e.min) +
+            '&end=' + Math.round(e.max) + '&callback=?', function (data) {
+
+            chart.series[0].setData(data);
+            chart.hideLoading();
+        });
+    }
+
+    // See source code from the JSONP handler at https://github.com/highcharts/highcharts/blob/master/samples/data/from-sql.php
+    $.getJSON('https://www.highcharts.com/samples/data/from-sql.php?callback=?', function (data) {
+
+        // Add a null value for the end date
+        data = [].concat(data, [[Date.UTC(2011, 9, 14, 19, 59), null, null, null, null]]);
+
+        // create the chart
+        $('#c3').highcharts('StockChart', {
+            chart : {
+                type: 'candlestick',
+                zoomType: 'x'
+            },
+
+            navigator : {
+                adaptToUpdatedData: false,
+                series : {
+                    data : data
+                }
+            },
+
+            scrollbar: {
+                liveRedraw: false
+            },
+
+            title: {
+                text: 'AAPL history by the minute from 1998 to 2011'
+            },
+
+            subtitle: {
+                text: 'Displaying 1.7 million data points in Highcharts Stock by async server loading'
+            },
+
+            rangeSelector : {
+                buttons: [{
+                    type: 'hour',
+                    count: 1,
+                    text: '1h'
+                }, {
+                    type: 'day',
+                    count: 1,
+                    text: '1d'
+                }, {
+                    type: 'month',
+                    count: 1,
+                    text: '1m'
+                }, {
+                    type: 'year',
+                    count: 1,
+                    text: '1y'
+                }, {
+                    type: 'all',
+                    text: 'All'
+                }],
+                inputEnabled: false, // it supports only days
+                selected : 4 // all
+            },
+
+            xAxis : {
+                events : {
+                    afterSetExtremes : afterSetExtremes
+                },
+                minRange: 3600 * 1000 // one hour
+            },
+
+            yAxis: {
+                floor: 0
+            },
+
+            series : [{
+                data : data,
+                dataGrouping: {
+                    enabled: false
+                }
+            }]
+        });
+    });
+
     $.each(names, function (i, name) {
 
         $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
@@ -151,7 +244,7 @@ $(function() {
 });
 
 
-$('.sortable').droppable({
+$('.apc_column--content').droppable({
     drop: function(event, ui)
     {
         function timeout() {
@@ -161,5 +254,20 @@ $('.sortable').droppable({
         window.setTimeout(function() {
             $(window).resize();
         },100);
+    }
+});
+
+
+$(".apc_column--content").sortable({
+    placeholder: 'apc_drop-placeholder-blocked',
+    forcePlaceholderSize: true,
+    items: '.apc_inner_item',
+    handle : '.widgetTitle',
+    connectWith: ".apc_column--content",
+    tolerance: "pointer",
+    dropOnEmpty: true,
+    distance: 0.5,
+    stop: function(event, ui) {
+        // that.droppedItem(ui.item, ui.item.index());
     }
 });
