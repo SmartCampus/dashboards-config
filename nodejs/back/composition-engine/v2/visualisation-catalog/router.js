87173099e3ec0ed82e61de92af3d966b9e0ff65e
Marc Karassev <marc.karassev@yahoo.fr>
2016-02-26 14:09:45 +0100

    documented API

--- nodejs/back/composition-engine/v2/visualisation-catalog/router.js
+++ nodejs/back/composition-engine/v2/visualisation-catalog/router.js
@@ -8,12 +8,30 @@ var router = require("express").Router(),
 	catalog = require("./catalog"),
     logger = require("./logger");
 /**
- * // TODO
- * @param  {[type]} req  [description]
- * @param  {[type]} res) {	var        grouped [description]
- * @return {[type]}      [description]
+ * Handles GET requests on /charts path. Returns matching charts according to
+ * given requirements. Expects "grouped" and "functions" query parameters.
+ * Example: GET /charts?grouped=true&functions=Comparisons-Proportions
+ *
+ * @param  boolean 	grouped 	whether the chart has to handle multiple data
+ *                           		sources or not	
+ * @param  string 	functions 	a list of visualisation functions where function
+ *                            		identifiers are delimited by a '-' character
+ * @return [object]				an array of charts sorted from the most matching
+ *                        			to the less one according to a rating
+ *                        			criteria, ex:
+ *                        				[
+ *                        					{
+ *                        						"name": "Line Graph",
+ *                        						"rating": 4
+ *                        					},
+ *                        					{
+ *                        						"name": "Dot Map",
+ *                        						"rating": 5
+ *                        					}
+ *                        					...
+ *                        				]
  */
-router.post("/functions", function (req, res) {
+router.get("/charts", function (req, res) {
 	var grouped = req.query.grouped, functions = req.query.functions.split(/-/);
 
 	logger.debug(grouped);
