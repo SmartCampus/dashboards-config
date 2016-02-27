b52621b29d315f8aa7e3788fba9a77f324243c1b
Marc Karassev <marc.karassev@yahoo.fr>
2016-02-27 23:27:33 +0100

    composition logic -> done to test

--- nodejs/back/composition-engine/v2/engine.js
+++ nodejs/back/composition-engine/v2/engine.js
@@ -4,6 +4,8 @@
  * Module handling the needs, widgets and sensors composition logic.
  */
 
+"use strict";
+
 var catalogRequester = require("./visualization_catalog_requester"),
 	async = require("async"),
 	logger = require("./logger");
@@ -11,6 +13,77 @@ var catalogRequester = require("./visualization_catalog_requester"),
 var NEEDS, WIDGETS;
 
 // has to restrict needs in order to absolutely match a chart
+function compose(needs, sensors, callback) {
+	var grouped = sensors.length > 1, compatibleWidgets = [];
+
+	catalogRequester.getRatedCharts(needs, grouped, function (err, ratedcharts) {
+		if (err) {
+			logger.warn(err);
+			callback(err, null);
+		}
+		else {
+			compatibleWidgets = ratedcharts.map(function (ratedchart) {
+				return adaptWidgetName(ratedchart.chart);
+			});
+			callback(null, {
+				needs: Array.from(findCompatibleNeeds(needs)),
+				widgets: compatibleWidgets
+			});
+		}
+	});
+}
+
+function findCompatibleNeeds(needs) {
+	var widget, functions, compatibleNeeds = new Set();
+
+	for (var i in WIDGETS) {
+		widget = WIDGETS[i];
+		if (widgetContainsNeeds(widget, needs, grouped)) {
+			functions = grouped ? widget.whenGroupedFunctions : widget.functions; 
+			functions.forEach(function (f) {
+				compatibleNeeds.add(f);
+			});
+		}
+	}
+}
+
+function widgetContainsNeeds(widget, needs, grouped) {
+	var containedNeeds = grouped ? widget.whenGroupedFunctions : widget.functions;
+
+	for (var i in needs) {
+		if (!containedNeeds.find(function (containedNeed) {
+			return containedNeed === needs[i];
+		})) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function getWidgetByName(widgetName) {
+	return WIDGETS.find(function (widget) {
+		return widget.name === widgetName;
+	});
+}
+
+function adaptWidgetName(widgetName) {
+	switch(widgetName.toLowerCase()) {
+		case "line graph": return "line";
+		case "bar chart": return "column";
+		case "pie chart": return "pieChart";
+		case "scatterplot": return "scatterplot";
+		case "dot map": return "map";
+		case "boolean": return "boolean";
+		case "line and bar chart": return "mix";
+		default: return undefined;
+	}
+}
+
+function needExists(need) {
+	return NEEDS.find(function (element) {
+		return need.toLowerCase() === element.toLowerCase();
+	}) ? true : false;
+}
 
 function getNeeds() {
 	return NEEDS;
@@ -60,6 +133,8 @@ function init(callback) {
 
 // Exports
 
+exports.compose = compose;
+exports.needExists = needExists;
 exports.getNeeds = getNeeds;
 exports.getWidgets = getWidgets;
 exports.init = init;
