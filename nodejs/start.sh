262bb98e671e9071c9a9339f49ff33ed5db9235c
Marc Karassev <marc.karassev@yahoo.fr>
2016-03-02 15:17:12 +0100

    màj scripts

--- nodejs/start.sh
+++ nodejs/start.sh
@@ -5,5 +5,6 @@ cd "$parent_path"
 forever start back/format-data-service/app.js
 forever start back/sensor-containers-API/app.js
 forever start back/dashboard-generator/app.js
+forever start back/composition-engine/visualization-catalog/app.js
 forever start back/composition-engine/app.js
 forever start front/app.js
